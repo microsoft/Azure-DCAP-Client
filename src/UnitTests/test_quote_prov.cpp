1ecf09cd029f56528aabf603a2ac2c1d217a90b1
Johnathan Chow <jochow@microsoft.com>
2019-06-18 11:00:12 -0700

    Undo VS pretty

--- src/UnitTests/test_quote_prov.cpp
+++ src/UnitTests/test_quote_prov.cpp
@@ -6,21 +6,18 @@
 #include "ext\intel\sgx_ql_lib_common.h"
 #include "local_cache.h"
 
-#if defined(__LINUX__)
-#include <dlfcn.h>
-#include <tgmath.h>
 #include <cstdio>
 #include <cstring>
 #include <ctime>
 #include <memory>
+
+#if defined(__LINUX__)
+#include <tgmath.h>
+#include <dlfcn.h>
 #else
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include <windows.h>
-#include <memory>
 #endif
 
 typedef quote3_error_t (*sgx_ql_get_quote_config_t)(
@@ -60,8 +57,7 @@ static void Log(sgx_ql_log_level_t level, const char* message)
 #if defined __LINUX__
 static void* LoadFunctions()
 {
-    // if this assert fails, be sure libdcap_quoteprov.so is in your
-    // LD_LIBRARY_PATH
+    // if this assert fails, be sure libdcap_quoteprov.so is in your LD_LIBRARY_PATH
 
     void* library = dlopen("libdcap_quoteprov.so", RTLD_NOW);
     printf(dlerror());
@@ -71,26 +67,19 @@ static void* LoadFunctions()
         abort();
     }
 
-    sgx_ql_free_revocation_info =
-        reinterpret_cast<sgx_ql_free_revocation_info_t>(
-            dlsym(library, "sgx_ql_free_revocation_info"));
+    sgx_ql_free_revocation_info = reinterpret_cast<sgx_ql_free_revocation_info_t>(dlsym(library, "sgx_ql_free_revocation_info"));
     assert(sgx_ql_free_revocation_info);
 
-    sgx_ql_get_revocation_info = reinterpret_cast<sgx_ql_get_revocation_info_t>(
-        dlsym(library, "sgx_ql_get_revocation_info"));
+    sgx_ql_get_revocation_info = reinterpret_cast<sgx_ql_get_revocation_info_t>(dlsym(library, "sgx_ql_get_revocation_info"));
     assert(sgx_ql_get_revocation_info);
 
-    sgx_ql_free_quote_config = reinterpret_cast<sgx_ql_free_quote_config_t>(
-        dlsym(library, "sgx_ql_free_quote_config"));
+    sgx_ql_free_quote_config = reinterpret_cast<sgx_ql_free_quote_config_t>(dlsym(library, "sgx_ql_free_quote_config"));
     assert(sgx_ql_free_quote_config);
 
-    sgx_ql_get_quote_config = reinterpret_cast<sgx_ql_get_quote_config_t>(
-        dlsym(library, "sgx_ql_get_quote_config"));
+    sgx_ql_get_quote_config = reinterpret_cast<sgx_ql_get_quote_config_t>(dlsym(library, "sgx_ql_get_quote_config"));
     assert(sgx_ql_get_quote_config);
 
-    sgx_ql_set_logging_function =
-        reinterpret_cast<sgx_ql_set_logging_function_t>(
-            dlsym(library, "sgx_ql_set_logging_function"));
+    sgx_ql_set_logging_function = reinterpret_cast<sgx_ql_set_logging_function_t>(dlsym(library, "sgx_ql_set_logging_function"));
     assert(sgx_ql_set_logging_function);
 
     return library;
@@ -98,38 +87,28 @@ static void* LoadFunctions()
 #else
 static HINSTANCE LoadFunctions()
 {
-    // https://stackoverflow.com/questions/27220/how-to-convert-stdstring-to-lpcwstr-in-c-unicode
     std::wstring s = L"dcap_quoteprov.dll";
     HINSTANCE hLibCapdll = LoadLibrary(s.c_str());
     if (hLibCapdll == NULL)
     {
-        DWORD error = GetLastError();
-        std::cout << error << std::endl;
-
-        //Log(SGX_QL_LOG_ERROR, error);
-        exit(0);
+        DWORD error = GetLastError(); 
+        Log(SGX_QL_LOG_ERROR , std::to_string(error).c_str());
+        abort();
     }
 
-    sgx_ql_free_revocation_info =
-        reinterpret_cast<sgx_ql_free_revocation_info_t>(
-            GetProcAddress(hLibCapdll, "sgx_ql_free_revocation_info"));
+    sgx_ql_free_revocation_info = reinterpret_cast<sgx_ql_free_revocation_info_t>(GetProcAddress(hLibCapdll, "sgx_ql_free_revocation_info"));
     assert(sgx_ql_free_revocation_info);
 
-    sgx_ql_get_revocation_info = reinterpret_cast<sgx_ql_get_revocation_info_t>(
-        GetProcAddress(hLibCapdll, "sgx_ql_get_revocation_info"));
+    sgx_ql_get_revocation_info = reinterpret_cast<sgx_ql_get_revocation_info_t>(GetProcAddress(hLibCapdll, "sgx_ql_get_revocation_info"));
     assert(sgx_ql_get_revocation_info);
 
-    sgx_ql_free_quote_config = reinterpret_cast<sgx_ql_free_quote_config_t>(
-        GetProcAddress(hLibCapdll, "sgx_ql_free_quote_config"));
+    sgx_ql_free_quote_config = reinterpret_cast<sgx_ql_free_quote_config_t>(GetProcAddress(hLibCapdll, "sgx_ql_free_quote_config"));
     assert(sgx_ql_free_quote_config);
 
-    sgx_ql_get_quote_config = reinterpret_cast<sgx_ql_get_quote_config_t>(
-        GetProcAddress(hLibCapdll, "sgx_ql_get_quote_config"));
+    sgx_ql_get_quote_config = reinterpret_cast<sgx_ql_get_quote_config_t>(GetProcAddress(hLibCapdll, "sgx_ql_get_quote_config"));
     assert(sgx_ql_get_quote_config);
 
-    sgx_ql_set_logging_function =
-        reinterpret_cast<sgx_ql_set_logging_function_t>(
-            GetProcAddress(hLibCapdll, "sgx_ql_set_logging_function"));
+    sgx_ql_set_logging_function = reinterpret_cast<sgx_ql_set_logging_function_t>(GetProcAddress(hLibCapdll, "sgx_ql_set_logging_function"));
     assert(sgx_ql_set_logging_function);
 
     return hLibCapdll;
@@ -144,39 +123,14 @@ static void GetCertsTest()
     TEST_START();
 
     // Setup the input (choose an arbitrary Azure server)
-    uint8_t qe_id[16] = {0x00,
-                         0xbd,
-                         0x4b,
-                         0x28,
-                         0x79,
-                         0xd5,
-                         0xa2,
-                         0x76,
-                         0x4a,
-                         0x96,
-                         0x4a,
-                         0xb9,
-                         0x90,
-                         0x90,
-                         0x8b,
-                         0x67};
-
-    sgx_cpu_svn_t cpusvn = {0x05,
-                            0x05,
-                            0x02,
-                            0x05,
-                            0xff,
-                            0x80,
-                            0x01,
-                            0x00,
-                            0x00,
-                            0x00,
-                            0x00,
-                            0x00,
-                            0x00,
-                            0x00,
-                            0x00,
-                            0x00};
+    uint8_t qe_id[16] = {
+        0x00, 0xbd, 0x4b, 0x28, 0x79, 0xd5, 0xa2, 0x76,
+        0x4a, 0x96, 0x4a, 0xb9, 0x90, 0x90, 0x8b, 0x67
+    };
+    sgx_cpu_svn_t cpusvn = {
+        0x05, 0x05, 0x02, 0x05, 0xff, 0x80, 0x01, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
 
     sgx_isv_svn_t pcesvn = 6;
 
@@ -189,25 +143,12 @@ static void GetCertsTest()
 
     // Just sanity check a few fields. Parsing the certs would require a big
     // dependency like OpenSSL that we don't necessarily want.
-    constexpr sgx_cpu_svn_t CPU_SVN_MAPPED = {0x05,
-                                              0x05,
-                                              0x02,
-                                              0x04,
-                                              0x01,
-                                              0x80,
-                                              0x01,
-                                              0x00,
-                                              0x00,
-                                              0x00,
-                                              0x00,
-                                              0x00,
-                                              0x00,
-                                              0x00,
-                                              0x00,
-                                              0x00};
-    assert(
-        0 ==
-        memcmp(&CPU_SVN_MAPPED, &config->cert_cpu_svn, sizeof(CPU_SVN_MAPPED)));
+    constexpr sgx_cpu_svn_t CPU_SVN_MAPPED = {
+        0x05, 0x05, 0x02, 0x04, 0x01, 0x80, 0x01, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+
+    assert(0 == memcmp(&CPU_SVN_MAPPED, &config->cert_cpu_svn, sizeof(CPU_SVN_MAPPED)));
     assert(6 == config->cert_pce_isv_svn);
     assert(SGX_QL_CONFIG_VERSION_1 == config->version);
     assert(0 < config->cert_data_size);
@@ -225,13 +166,10 @@ static void GetCrlTest()
 {
     TEST_START();
 
-    static constexpr uint8_t TEST_FMSPC[] = {
-        0x00, 0x90, 0x6E, 0xA1, 0x00, 0x00};
+    static constexpr uint8_t TEST_FMSPC[] = {0x00, 0x90, 0x6E, 0xA1, 0x00, 0x00};
 
     // This is the CRL DP used by Intel for leaf certs
-    static const char* TEST_CRL_URL =
-        "https://api.trustedservices.intel.com/sgx/certification/v1/"
-        "pckcrl?ca=processor";
+    static const char* TEST_CRL_URL = "https://api.trustedservices.intel.com/sgx/certification/v1/pckcrl?ca=processor";
 
     sgx_ql_get_revocation_info_params_t params = {
         SGX_QL_REVOCATION_INFO_VERSION_1,
@@ -269,10 +207,7 @@ extern void QuoteProvTests()
     // First pass: Get the data from the service, no cache allowed
     //
 
-    setenv(
-        "AZDCAP_BASE_CERT_URL",
-        "https://global.acccache.azure.net/sgx/certificates",
-        1);
+    setenv("AZDCAP_BASE_CERT_URL", "https://global.acccache.azure.net/sgx/certificates", 1);
     local_cache_clear();
 
     start = std::clock();
@@ -293,9 +228,7 @@ extern void QuoteProvTests()
     // Ensure that there is a signficiant enough difference between the cert
     // fetch to the end point and cert fetch to local cache and that local cache
     // call is fast enough
-    assert(
-        fabs(duration_curl - duration_local) > 0.0001 &&
-        duration_local < 0.001);
+    assert(fabs(duration_curl - duration_local) > 0.0001 && duration_local < 0.001);
 
     dlclose(library);
 }
@@ -309,8 +242,7 @@ extern void QuoteProvTests()
     //
     // First pass: Get the data from the service, no cache allowed
     //
-    _putenv("AZDCAP_BASE_CERT_URL=https://global.acccache.azure.net/sgx/"
-            "certificates");
+    _putenv("AZDCAP_BASE_CERT_URL=https://global.acccache.azure.net/sgx/certificates");
 
     GetCertsTest();
     GetCrlTest();
