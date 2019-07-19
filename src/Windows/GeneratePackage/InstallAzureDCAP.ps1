5ff4ba1fe3d98b4cc1c92d8cb5d936850c6da3f1
msft-jonnadul <46332948+msft-jonnadul@users.noreply.github.com>
2019-07-19 16:47:25 -0700

    Updated scripts to reflect libcurl independences

--- src/Windows/GeneratePackage/InstallAzureDCAP.ps1
+++ src/Windows/GeneratePackage/InstallAzureDCAP.ps1
@@ -9,23 +9,8 @@ if (-not (Test-Path -Path $localPath))
 else
 {
     Copy-Item "..\dll\dcap_quoteprov.dll" -Destination $localPath
-    Copy-Item "..\dll\libcurl.dll" -Destination $localPath
-    Copy-Item "..\dll\libeay32.dll" -Destination $localPath
-    Copy-Item "..\dll\libssh2.dll" -Destination $localPath
-    Copy-Item "..\dll\ssleay32.dll" -Destination $localPath
-    Copy-Item "..\dll\zlib.dll" -Destination $localPath
-    Copy-Item "..\crt\curl-ca-bundle.crt" -Destination $localPath
     
-    $env:Path += ";" + $localPath
+    $newPath = $env:Path + ";$localPath"
 
-    [Environment]::SetEnvironmentVariable
-     ("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-
-    if ((-not (Test-Path -Path "$env:windir\System32\msvcp140.dll")) -or
-        (-not (Test-Path -Path "$env:windir\System32\msvcr110.dll")) -or
-        (-not (Test-Path -Path "$env:windir\System32\vcruntime140.dll")))
-    {
-        Write-Host "[WARNING]: Not all dependent v110 and v140 runtimes seem
-        to be present on system. Please ensure that you have those installed."
-    }
+    Set-ItemProperty -path 'hklm:\system\currentcontrolset\control\session manager\environment' -Name Path -Value $NewPath
 }
--- src/Windows/GeneratePackage/README.txt
+++ src/Windows/GeneratePackage/README.txt
@@ -3,21 +3,8 @@
 Azure DCAP is a plugin library component to the Intel DCAP for interfacing with
 the Azure PCK Caching Service.
 
-# VCRuntime Dependencies
-Please ensure that you have both the VC++ v110 and v140 runtimes installed
-on your system.
-
-VC++ Runtime v110: https://www.microsoft.com/en-us/download/details.aspx?id=30679
-VC++ Runtime v140: https://www.microsoft.com/en-us/download/details.aspx?id=53840
-
 # Nuget Package Layout
 - dll    - dcap_quoteprovider.dll : Azure DCAP for Windows DLL
-         - libcurl.dll            : List of dependent DLLs
-         - libeay32.dll           
-         - libssh2.dll
-         - ssleay32.dll
-         - zlib.dll
-- crl    - curl-ca-bundle.crt     : Global Root CA
 - script - InstallAzureDCAP.ps1   : Script to install Azure DCAP
 
 # Azure DCAP Installation
