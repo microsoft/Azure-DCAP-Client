e81e24a84e1b85d574333e90824943cdbc711fc4
Derek Schumacher <deschuma@microsoft.com>
2020-07-22 11:47:47 -0700

    Test change for PR

--- src/Windows/curl_easy.cpp
+++ src/Windows/curl_easy.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+// Licensed under the MIT License.\
+// nop
 
 #include "curl_easy.h"
 #include <cassert>
