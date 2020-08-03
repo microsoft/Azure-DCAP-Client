64f0e5dd5cb7a79dc4c3883749a6187110f48925
Wei Wang <wewang3@microsoft.com>
2020-08-03 16:48:22 -0700

    add buildLinux and change build.cmd file

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,8 +1,5 @@
-cd source
+cd ./../src/Windows
 
-REM Create and cd into build directory
-mkdir build 
-cd build 
-
-REM Enable cmake context and build binaries for signing
-vcvars64.bat && cmake .. -G Ninja -DNUGET_PACKAGE_PATH=C:\Downloads\prereqs\nuget -DCPACK_GENERATOR=NuGet -DCMAKE_BUILD_TYPE=Release -DBUILD_ENCLAVES=ON -DLVI_MITIGATION=ControlFlow -DHAS_QUOTE_PROVIDER=ON && ninja -j 1 -v
+ ./get-prereqs.ps1
+ 
+ ./dll/build.ps1 -BuildType Release
---
+++ .pipelines/buildLinux.cmd
@@ -0,0 +1,5 @@
+cd ./../src/Linux
+ 
+ ./configure
+ 
+ make
