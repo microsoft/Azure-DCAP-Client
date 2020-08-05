8f7e7652d6049cfb8ac5c6288c44aecab5ac25a4
Wei Wang <wewang3@microsoft.com>
2020-08-04 21:21:04 -0700

    change build.cmd file

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,3 +1,3 @@
 
 REM Enable cmake context and build binaries for signing
-vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 "%~dp0\..\src\Windows\dcap_provider.sln"
+vcvars64.bat && cd %~dp0\..\src\Windows\dll && powershell.exe -ExecutionPolicy Bypass -Command "{& [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;  \'Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile .\\nuget.exe\';}" && nuget.exe restore dcap_provider.vcxproj -PackagesDirectory packages && msbuild -p:Configuration=Release /p:Platform=x64 "%~dp0\..\src\Windows\dcap_provider.sln"
