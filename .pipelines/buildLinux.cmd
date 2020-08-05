e2dab6618f9164c7bb9b1c0283a301c9ae8ea630
Wei Wang <wewang3@microsoft.com>
2020-08-04 21:41:52 -0700

    changed build.cmd and buildLinux.cmd file

--- .pipelines/buildLinux.cmd
+++ .pipelines/buildLinux.cmd
@@ -4,12 +4,3 @@ sudo apt-get install sudo libcurl4-openssl-dev wget -y
 cd %~dp0/../src/Linux
 -buildpackage -us -uc
 dpkg -i %~dp0/../src/az-dcap-client_*_amd64.deb
-sudo apt-get update
-sudo apt-get install -y open-enclave
-. /opt/openenclave/share/openenclave/openenclaverc
-cp -r /opt/openenclave/share/openenclave/samples/ ~/samples
-for DIR in \$(find ~/samples/* -maxdepth 0 -type d); do
-    cd \$DIR
-    make build
-    make run
-done
