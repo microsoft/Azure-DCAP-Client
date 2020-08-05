c16e7e66e98edee862e57ea8c9e3fd3e43c41040
Wei Wang <wewang3@microsoft.com>
2020-08-05 10:20:02 -0700

    add nuget

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,3 +1,5 @@
 
+nuget install %~dp0\packages.config -ExcludeVersion -Outputdirectory C:\Downloads\prereqs\nuget
+
 REM Enable cmake context and build binaries for signing
 vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 "%~dp0\..\src\Windows\dcap_provider.sln"
