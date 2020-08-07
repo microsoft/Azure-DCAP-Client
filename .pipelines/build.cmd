7d739707c1f3bf76d61ae0ec5b4f32b1c5c606f0
Wei Wang <wewang3@microsoft.com>
2020-08-07 16:32:17 -0700

    added nuget feeds

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,7 +1,6 @@
 
-# nuget install \packages.config -ExcludeVersion -Outputdirectory C:\Downloads\prereqs\nuget
 
 cd %~dp0..\src\Windows
 
-# REM Enable cmake context and build binaries for signing
+REM Enable cmake context and build binaries for signing
 vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 ".\dcap_provider.sln"
