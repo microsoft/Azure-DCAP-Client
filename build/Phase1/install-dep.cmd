338740d39beacb8eb2c0daad5dd8b2a155bc4373
Wei Wang <wewang3@microsoft.com>
2020-08-07 10:38:13 -0700

    change powershell dir

--- build/Phase1/install-dep.cmd
+++ build/Phase1/install-dep.cmd
@@ -21,7 +21,7 @@ nuget install %ScriptDir%\packages.config -ExcludeVersion -Outputdirectory %Pack
 
 
 
-powershell %~dp0\..\..\src\Windows\dll\get-prereqs.ps1
+powershell %ENCLAVE_BUILD_SOURCE_ROOT%\Windows\dll\get-prereqs.ps1
 
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 exit /b 0
