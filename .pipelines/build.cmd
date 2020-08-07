b15be7e1c359d01a90cbdacc4a9aa3511a410e9f
Wei Wang <wewang3@microsoft.com>
2020-08-07 12:02:30 -0700

    changed msbuild command

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,5 +1,7 @@
 
 # nuget install \packages.config -ExcludeVersion -Outputdirectory C:\Downloads\prereqs\nuget
 
+cd %~dp0\..\src\Windows
+
 # REM Enable cmake context and build binaries for signing
-vcvars64.bat && powershell -ExecutionPolicy Unrestricted -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -Command "%~dp0..\src\Windows\get-prereqs.ps1" && msbuild -p:Configuration=Release /p:Platform=x64 "%~dp0\..\src\Windows\dcap_provider.sln"
+vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 ".\dcap_provider.sln"
