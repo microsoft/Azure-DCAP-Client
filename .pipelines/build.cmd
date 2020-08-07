d019aa1fb4e6034c8960da622d12aa1d3b827f11
Wei Wang <wewang3@microsoft.com>
2020-08-07 10:11:08 -0700

    remove powershell command

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -3,7 +3,5 @@
 
 cd %~dp0\..\src\Windows\dll
 
-powershell -ExecutionPolicy Bypass -Command "{& [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;  \'Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile .\\nuget.exe\';}"
-
 # REM Enable cmake context and build binaries for signing
 vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 "%~dp0\..\src\Windows\dcap_provider.sln"
