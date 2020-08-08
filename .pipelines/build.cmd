db2cc867a401d4f29ea7e0785a5109434dd13a01
Wei Wang <wewang3@microsoft.com>
2020-08-07 21:08:12 -0700

    changed scripts

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,6 +1,3 @@
 
-
-cd %~dp0..\src\Windows
-
 REM Enable cmake context and build binaries for signing
-vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 ".\dcap_provider.sln"
+vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 "%~dp0..\src\Windows\dcap_provider.sln"
