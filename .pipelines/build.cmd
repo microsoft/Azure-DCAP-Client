3ae5a699fabffd120eb35a3a03ea3d16f8c4d21c
Wei Wang <wewang3@microsoft.com>
2020-08-10 12:58:25 -0700

    changed build.cmd dir

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,3 +1,4 @@
+cd %~dp0..\src
 
 REM Enable cmake context and build binaries for signing
-vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 "%~dp0..\src\Windows\dcap_provider.sln"
+vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 ".\Windows\dcap_provider.sln"
