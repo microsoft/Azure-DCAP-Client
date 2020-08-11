eff403a9d143c929f316de3b3e7b3e9dbae535e2
Wei Wang <wewang3@microsoft.com>
2020-08-11 14:11:50 -0700

    changed build and package script

--- build/Phase1/post-build.cmd
+++ build/Phase1/post-build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 echo After Build End
-echo Out: %~dp0\..\..\src\AttestationServices\Instance\Out
-dir %~dp0\..\..\src\AttestationServices\Instance\Out
-dir /s %~dp0\..\..\src\AttestationServices\Instance\Out\AttestationSgxEnclave
+echo Out: %~dp0\..\..\src\Windows\Out
+dir %~dp0\..\..\src\Windows\Out
+dir /s %~dp0\..\..\src\Windows\Out\AzureDCAPClient
