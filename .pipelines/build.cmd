d91dfa7ad4fdaa4fa9a68100bc76855e2e709b49
Wei Wang <wewang3@microsoft.com>
2020-08-11 12:12:27 -0700

    build vcxproj file

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,4 +1,4 @@
 cd %~dp0..\src\Windows
 
 REM Enable cmake context and build binaries for signing
-vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 ".\dcap_provider.sln"
+vcvars64.bat && msbuild -p:Configuration=Release /p:Platform=x64 ".\dll\dcap_provider.vcxproj"
