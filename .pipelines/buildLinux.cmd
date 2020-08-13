95a859495ff1604a187e9874e4f63e059cc6d96a
Wei Wang <wewang3@microsoft.com>
2020-08-13 14:12:08 -0700

    change dpkg dir

--- .pipelines/buildLinux.cmd
+++ .pipelines/buildLinux.cmd
@@ -3,13 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
-cd $DIR/../src/Linux/
+cd $DIR/../../src/Linux/
 
 apt-get install libssl-dev
 apt install libcurl4-openssl-dev
 apt-get install pkg-config
-
-git submodule update –recursive –init
 
 ./configure
 
--- build/Phase2/install_prereqs.cmd
+++ build/Phase2/install_prereqs.cmd
@@ -10,5 +10,5 @@ apt-get install pkg-config
 
 apt install build-essential
 
-apt-get install -y dh-virtualenv
+apt-get install -y debhelper dh-virtualenv
 
