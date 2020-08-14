7b7d437b041a8208ade3e0330fe6e95481abc2f8
Wei Wang <wewang3@microsoft.com>
2020-08-14 14:13:03 -0700

    changed cpoy buildOutputs

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -5,3 +5,4 @@ if "%VCToolsVersion%" EQU "" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2
 
 powershell -ExecutionPolicy Unrestricted -Command %~dp0..\src\Windows\dll\build.ps1 -BuildType Release -SkipRestore
  
+msbuild -p:Configuration=Release /p:Platform=x64 "%~dp0..\src\Windows\dcap_provider_tests\dcap_provider_tests.vcxproj"
