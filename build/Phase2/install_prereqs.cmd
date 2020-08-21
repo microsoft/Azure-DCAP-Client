2fe30b490909ef51e875d7ecd6a4bc2cede03bfe
Wei Wang <wewang3@microsoft.com>
2020-08-21 12:03:39 -0700

    add cmakefile

--- build/Phase2/install_prereqs.cmd
+++ build/Phase2/install_prereqs.cmd
@@ -12,9 +12,17 @@ apt install build-essential
 
 apt-get install -y debhelper dh-virtualenv
 
+apt-get update -y
 apt-get install -y libgtest-dev
 
 apt-get install -y cmake
+
+cd /usr/src/gtest
+cmake CMakeLists.txt
+make
+ 
+# copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
+cp *.a /usr/lib
 
 cd $DIR/../../src/Linux/
 
