274757beb6ab3f69b1f276a163ee776a7733da7b
Wei Wang <wewang3@microsoft.com>
2020-08-20 21:09:27 -0700

    update test code

--- src/Windows/gUnitTests/test_local_cache.cpp
+++ src/Windows/gUnitTests/test_local_cache.cpp
@@ -1,5 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 #include "pch.h"
-
 #undef NDEBUG // ensure that asserts are never compiled out
 #include <cassert>
 #include <cstdio>
@@ -11,45 +12,38 @@
 #include <windows.h>
 #endif
 
-#include "../../UnitTests/unit_test.h"
-#include "../../local_cache.h"
+#include "UnitTests/unit_test.h"
+#include "local_cache.h"
 
 static time_t now()
 {
     return time(nullptr);
 }
 
-template <typename ExceptionT>
-static void AssertException(void (*function)())
+//
+// Add an item to the cache and retrieve it.
+//
+static boolean AddGetItem()
 {
-    boolean foundException = false;
-    try
-    {
-        function();
-    }
-    catch (ExceptionT&)
-    {
-        foundException = true;
-        return;
-    }
-
-    ASSERT_TRUE(foundException);
-}
-
-TEST(testLocalCache, AddGetItem)
-{
-    local_cache_clear();
+    TEST_START();
 
     static const std::vector<uint8_t> data = {8, 6, 7, 5, 3, 0, 9};
     local_cache_add(__FUNCTION__, now() + 60, data.size(), data.data());
 
     auto retrieved = local_cache_get(__FUNCTION__);
-    ASSERT_TRUE(retrieved != nullptr);
-    ASSERT_TRUE(*retrieved == data);
+    assert(retrieved != nullptr);
+    assert(*retrieved == data);
+
+    TEST_PASSED();
+    return true;
 }
 
-TEST(testLocalCache, OverwriteCacheEntry)
+//
+// Add an item to the cache, then overwrite it
+//
+static boolean OverwriteCacheEntry()
 {
+    TEST_START();
 
     static const std::vector<uint8_t> data1 = {1, 1, 1, 1};
     local_cache_add(__FUNCTION__, now() + 60, data1.size(), data1.data());
@@ -58,22 +52,38 @@ TEST(testLocalCache, OverwriteCacheEntry)
     local_cache_add(__FUNCTION__, now() + 60, data2.size(), data2.data());
 
     auto retrieved = local_cache_get(__FUNCTION__);
-    ASSERT_TRUE(*retrieved == data2);
+    assert(*retrieved == data2);
 
+    TEST_PASSED();
+    return true;
 }
 
-TEST(testLocalCache, VerifyClearCache)
+//
+// Add an item to the cache, clear the cache, then ensure the entry is gone
+//
+static boolean VerifyClearCache()
 {
+    TEST_START();
+
     static const uint8_t data[] = "stuff goes here";
     local_cache_add(__FUNCTION__, now() + 60, sizeof(data), data);
 
-    ASSERT_TRUE(nullptr != local_cache_get(__FUNCTION__));
+    assert(nullptr != local_cache_get(__FUNCTION__));
     local_cache_clear();
-    ASSERT_TRUE(nullptr == local_cache_get(__FUNCTION__));
+    assert(nullptr == local_cache_get(__FUNCTION__));
+
+    TEST_PASSED();
+    return true;
 }
 
-TEST(testLocalCache, VerifyExpiryWorks)
+//
+// Add an item to the cache with a short expiry, then ensure it actually
+// expires.
+//
+static boolean VerifyExpiryWorks()
 {
+    TEST_START();
+
     int expiry_seconds = 2;
 
     // add an entry with expiry shortly in the future
@@ -81,7 +91,7 @@ TEST(testLocalCache, VerifyExpiryWorks)
     local_cache_add(__FUNCTION__, now() + expiry_seconds, sizeof(data), data);
 
     // ensure the data is there
-    ASSERT_TRUE(nullptr != local_cache_get(__FUNCTION__));
+    assert(nullptr != local_cache_get(__FUNCTION__));
 
     // wait for expiry, after which the data should be gone
 
@@ -91,11 +101,35 @@ TEST(testLocalCache, VerifyExpiryWorks)
     Sleep(expiry_seconds * 1000);
 #endif
 
-    ASSERT_TRUE(nullptr == local_cache_get(__FUNCTION__));
+    assert(nullptr == local_cache_get(__FUNCTION__));
+
+    TEST_PASSED();
+
+    return true;
 }
 
-TEST(testLocalCache, InvalidParams)
+template <typename ExceptionT>
+static void AssertException(void (*function)())
 {
+    try
+    {
+        function();
+    }
+    catch (ExceptionT&)
+    {
+        return;
+    }
+
+    assert(!"Expected exception was not thrown");
+}
+
+//
+// Verify invalid parameter handling
+//
+static boolean InvalidParams()
+{
+    TEST_START();
+
     static const uint8_t data[] = "test data";
 
     AssertException<std::runtime_error>(
@@ -108,11 +142,19 @@ TEST(testLocalCache, InvalidParams)
         [] { local_cache_add(__FUNCTION__, now(), sizeof(data), nullptr); });
 
     AssertException<std::runtime_error>([] { local_cache_get(""); });
+
+    TEST_PASSED();
+    return true;
 }
 
-
-TEST(testLocalCache, ThreadSafetyTest)
+//
+// Spawns multiple threads, intentionally creating a lot of contention for
+// a single cache entry.
+//
+static boolean ThreadSafetyTest()
 {
+    TEST_START();
+
     // Pre-fill a data vector that's of sufficient size that we
     // are likely to get conflicts between threads.
     std::vector<uint8_t> data(64 * 1024);
@@ -135,8 +177,8 @@ TEST(testLocalCache, ThreadSafetyTest)
         for (unsigned i = 0; i < THREAD_LOOP_COUNT; ++i)
         {
             auto retrieved = local_cache_get(ID);
-            ASSERT_TRUE(retrieved != nullptr);
-            ASSERT_TRUE(*retrieved == data);
+            assert(retrieved != nullptr);
+            assert(*retrieved == data);
         }
     };
 
@@ -165,4 +207,39 @@ TEST(testLocalCache, ThreadSafetyTest)
     {
         t.join();
     }
+
+    TEST_PASSED();
+    return true;
+}
+
+TEST(testLocalCache, AddGetItem)
+{
+    local_cache_clear();
+
+    ASSERT_TRUE(AddGetItem());
+}
+
+TEST(testLocalCache, OverwriteCacheEntry)
+{
+    ASSERT_TRUE(OverwriteCacheEntry());
+}
+
+TEST(testLocalCache, VerifyClearCache)
+{
+    ASSERT_TRUE(VerifyClearCache());
+}
+
+TEST(testLocalCache, VerifyExpiryWorks)
+{
+    ASSERT_TRUE(VerifyExpiryWorks());
+}
+
+TEST(testLocalCache, InvalidParams)
+{
+    ASSERT_TRUE(InvalidParams());
+}
+
+TEST(testLocalCache, ThreadSafetyTest)
+{
+    ASSERT_TRUE(ThreadSafetyTest());
 }
