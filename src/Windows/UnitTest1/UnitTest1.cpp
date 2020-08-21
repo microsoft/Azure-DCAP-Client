c52f7fa3b30002ed4821dcfaf877a5b0732a9743
Wei Wang <wewang3@microsoft.com>
2020-08-21 11:47:58 -0700

    not need pch head

---
+++ src/Windows/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
