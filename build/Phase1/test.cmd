399c43dd4401d097bfaf106c92daba55a6624eb3
Wei Wang <wewang3@microsoft.com>
2020-09-09 21:54:15 -0700

    changed to vs2019 and changed exe path

--- build/Phase1/test.cmd
+++ build/Phase1/test.cmd
@@ -2,18 +2,20 @@
 
 REM Configure Visual Studio
 if not defined DevEnvDir (
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0
 )
 
 REM Directory
-SET dir=%~dp0..
+SET dir=%~dp0..\..
 
 REM GoogleTestAdapter
 FOR /F "tokens=*" %%i IN ('powershell -Command "& {Write-Host ((Get-ChildItem -Path (Join-Path '%DevEnvDir%' Extensions) -Recurse | Where-Object { $_.Name -eq 'GoogleTestAdapter.TestAdapter.dll' } | Select-Object -First 1).DirectoryName)}"') do @SET gta=%%i
 
+echo %gta%
+
 REM Test
 vstest.console.exe^
-  %dir%\build\x64\test\unit\Release\UnitTests.exe^
+  %dir%\src\Windows\x64\Release\UnitTests.exe^
   /TestAdapterPath:"%gta%"^
   /Platform:x64^
   /logger:trx;LogFileName=Release.trx
