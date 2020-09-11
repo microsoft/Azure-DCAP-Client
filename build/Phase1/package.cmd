e6609513fca636cee92e2d746d690de25fa84539
Wei Wang <wewang3@microsoft.com>
2020-09-11 14:12:20 -0700

    changed package command

--- build/Phase1/package.cmd
+++ build/Phase1/package.cmd
@@ -2,11 +2,6 @@
 
 REM Packaging script that generates a Nuget
 
-REM Run the Environment variables setup script
-call %~dp0setenv.cmd
+powershell -ExecutionPolicy Unrestricted -Command "package.ps1"
 
-REM Get the current script directory
-SET ScriptDir=%~dp0.
-
-REM Invoke the Powershell packaging script
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Unrestricted -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -Command "%ScriptDir%\package.ps1"
+SET NuGetPackageRoot=
