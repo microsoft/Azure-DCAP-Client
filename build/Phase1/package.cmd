904de118afba5e34b313d989abf3c21ffd8509c4
Wei Wang <wewang3@microsoft.com>
2020-09-11 15:03:51 -0700

    changed package output dir

--- build/Phase1/package.cmd
+++ build/Phase1/package.cmd
@@ -5,7 +5,7 @@ REM Packaging script that generates a Nuget
 cd %~dp0..\..\src\Windows
 
 REM Create the nuget package and bail if it fails to construct it
-nuget pack ".\GeneratePackage\Azure.DCAP.Windows.nuspec" -Symbols -SymbolPackageFormat snupkg
+nuget pack ".\GeneratePackage\Azure.DCAP.Windows.nuspec" -Symbols -SymbolPackageFormat snupkg -OutputDirectory .\Out\packages
 
 REM powershell -ExecutionPolicy Unrestricted -Command "package.ps1"
 
