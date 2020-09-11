02fa2c3cb206d013c44eb5121e5c9e21ea1093ff
Wei Wang <wewang3@microsoft.com>
2020-09-10 17:21:28 -0700

    copy gtestadapter package

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -4,4 +4,10 @@ REM Enable cmake context and build binaries for signing
 if "%VCToolsVersion%" EQU "" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
 
 powershell -ExecutionPolicy Unrestricted -Command %~dp0..\src\Windows\dll\build.ps1 -BuildType Release -SkipRestore
+
+cd %~dp0..\src\Windows\x64\Release
+
+mkdir GoogleTestAdapter
+
+cp -r %~dp0..\src\Windows\packages\GoogleTestAdapter.0.18.0\build\_common .\GoogleTestAdapter
  
