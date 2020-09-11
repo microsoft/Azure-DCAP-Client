4a73e4260d5f5bc038a7ebd7ecd428f6c45699a7
Wei Wang <wewang3@microsoft.com>
2020-09-10 21:50:51 -0700

    changed copy googletestadapter

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -5,7 +5,4 @@ if "%VCToolsVersion%" EQU "" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2
 
 powershell -ExecutionPolicy Unrestricted -Command %~dp0..\src\Windows\dll\build.ps1 -BuildType Release -SkipRestore
 
-cd %~dp0..\src\Windows\x64\Release
-
-cp -r %~dp0..\src\Windows\packages\GoogleTestAdapter.0.18.0\build\_common GoogleTestAdapter /E /H /C /I
  
