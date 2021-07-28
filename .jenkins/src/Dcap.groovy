0b755dc0baa4add943359680dfd586f4b0b07962
Gunjan Munjal <gumunjal@microsoft.com>
2021-07-27 21:21:23 -0700

    Updating the command to copy libgtest.a and libgtest_main.a

--- .jenkins/src/Dcap.groovy
+++ .jenkins/src/Dcap.groovy
@@ -117,17 +117,17 @@ def deleteRG(List resourceGroups, String imageName = "oetools-deploy:latest") {
 
 def emailJobStatus(String status) {
     emailext (
-        to: '$DEFAULT_RECIPIENTS',
-        subject: "[Jenkins Job ${status}] ${env.JOB_NAME} - ${env.BUILD_NUMBER}",
-        body: """
+      to: '$DEFAULT_RECIPIENTS',
+      subject: "[Jenkins Job ${status}] ${env.JOB_NAME} - ${env.BUILD_NUMBER}",
+      body: """
             <p>
             For additional logging details for this job please check: 
             <a href="${env.BUILD_URL}">${env.JOB_NAME} - ${env.BUILD_NUMBER}</a>
             </p>
             """,
-        recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
-        mimeType: 'text/html'
-        )
+      recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
+      mimeType: 'text/html'
+    )
 }
 
 /**
