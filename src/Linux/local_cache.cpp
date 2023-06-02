42dc56209d84bad676b11972a124380a018bf59f
FranciscoJavierOrtegaPalacios <111856750+FranciscoJavierOrtegaPalacios@users.noreply.github.com>
2023-06-02 18:51:23 +0100

    Bug fix

--- src/Linux/local_cache.cpp
+++ src/Linux/local_cache.cpp
@@ -268,7 +268,7 @@ static std::string get_file_name(const std::string& id)
     return g_cache_dirname + "/" + sha256(id);
 }
 
-std::wstring get_cached_file_location(const std::string& id){
+std::string get_cached_file_location(const std::string& id){
 	//Temporary duplicate code while I confirm that using get_file_name doesn't cause issues with locks
 	//return get_file_name(id);
 	return g_cache_dirname + "/" + sha256(id);
