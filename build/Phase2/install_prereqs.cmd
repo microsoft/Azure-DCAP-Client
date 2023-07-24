6c92d1e22eec9af016390f0ef33e44ca44cf81ac
Gunjan Munjal <gumunjal@microsoft.com>
2023-07-24 15:18:07 -0700

    updating build file.

--- build/Phase2/install_prereqs.cmd
+++ build/Phase2/install_prereqs.cmd
@@ -5,14 +5,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 apt-get install libssl-dev
-apt install libcurl4-openssl-dev
-apt-get install pkg-config
+apt-get update -y
+apt install libcurl4-openssl-dev -y
+apt-get install pkg-config -y
 
 add-apt-repository ppa:team-xbmc/ppa -y
 apt-get update -y
 apt-get install nlohmann-json3-dev
 
-apt install build-essential
+apt install build-essential -y
 
 apt-get install -y debhelper dh-virtualenv
 
@@ -23,6 +24,7 @@ apt-get install -y cmake
 cd /usr/src/gtest
 cmake CMakeLists.txt
 make
+cd lib
 # copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
 cp *.a /usr/lib
 
