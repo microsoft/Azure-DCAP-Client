5dc1dabe75d2dcd34af35d73e0ede752e8337881
Gunjan Munjal <gumunjal@microsoft.com>
2023-07-27 15:39:14 -0700

    Updating yaml file.

--- .github/workflows/scripts/buildcode.cmd
+++ .github/workflows/scripts/buildcode.cmd
@@ -4,7 +4,7 @@
 # Get the directory where this script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd $DIR/../../src/Linux/
+cd $DIR/../../../src/Linux/
 
 make clean
 ./configure
