a8646e71ef37446ebb19c2f5c6372741a9675d38
msft-gumunjal <81185076+msft-gumunjal@users.noreply.github.com>
2023-08-01 10:57:23 -0700

    Create codeql.yml (#188)
    * updating build file.
    
    * Commenting dpkg for testing.
    
    * Commenting auto detecting of language.
    
    * Adding script for code building
    
    * Adding script for code building
    
    * Removing extra lines.

---
+++ .github/workflows/scripts/buildcode.cmd
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Get the directory where this script resides
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR/../../../src/Linux/
+
+make clean
+./configure
+make
