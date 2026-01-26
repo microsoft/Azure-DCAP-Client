980753800659b143c7d186c906739e3676f46631
Gunjan Munjal <gumunjal@microsoft.com>
2026-01-26 15:56:26 +0530

    Updating DCAPCloneMain.ps1 file.

--- .github/windowsScripts/DCAPCloneMain.ps1
+++ .github/windowsScripts/DCAPCloneMain.ps1
@@ -3,8 +3,10 @@ Param(
     [Parameter(Mandatory=$true)][string]$branch
 )
 
-$command = "git clone -b $branch $repo C:/AzureDCAP  && "
-$command += "echo `"DCAP_Build_Step_Successfully_Completed`""
-
+# Delete old folder (quietly), then clone, then echo on success
 cmd.exe /c "rmdir /s /q C:\AzureDCAP"
+
+# Build a proper cmd.exe command line
+$command = "git clone -b $branch $repo ""C:\AzureDCAP"" && echo ""DCAP_Build_Step_Successfully_Completed"""
+
 cmd.exe /c $command
