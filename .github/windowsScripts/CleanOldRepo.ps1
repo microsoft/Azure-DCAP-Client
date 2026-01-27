8324a6b7487ce09889e2bbc8a6dcfff8496fa667
Gunjan Munjal <gumunjal@microsoft.com>
2026-01-27 10:34:17 +0530

    Added separate step to clean the old repo.

--- .github/windowsScripts/CleanOldRepo.ps1
+++ .github/windowsScripts/CleanOldRepo.ps1
@@ -1,4 +1,4 @@
 $command = "rmdir /s /q C:\AzureDCAP  && "
-$command += "echo `"Old_DCAP_Build_Step_Successfully_Cleaned"`"
+$command += "echo `"Old_DCAP_Build_Step_Successfully_Cleaned`""
 
 cmd.exe /c $command
